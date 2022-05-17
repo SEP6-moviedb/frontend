7e7dd4f59eb3ad1302dba8821d53236b9904373a
detb <57094519+detb@users.noreply.github.com>
2022-05-17 12:39:59 +0000

    Deploying to main from @ SEP6-moviedb/frontend@068f4b38a387430a37208cd9568abfe91b598dea 🚀

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  tmdbApiKey: 'd969c038879a912c97bceafc05ec99cd',
-  googleClientId: '554920019463-5k22uf5t28ni2mdgndqkfa67g5r9ka2h.apps.googleusercontent.com'
-};
