6b808ea4b38d4dccb7be0814c1abefae5c3cb128
patrickdurke <50921587+patrickdurke@users.noreply.github.com>
2022-05-17 14:08:23 +0200

    CanvasJS chart prototype

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -4,6 +4,10 @@ import { ApiHttpService } from 'src/app/services/api-http.service'
 import { AuthenticationService } from '../../services/authentication.service';
 import { Subscription } from "rxjs";
 import { ActivatedRoute } from '@angular/router';
+//import * as CanvasJS from './canvasjs.min';
+
+declare const CanvasJS: any;
+
 
 @Component({
   selector: 'app-statistics',
@@ -23,7 +27,32 @@ export class StatisticsComponent implements OnInit {
   ngOnInit(): void {
     this.routeSub = this.route.params.subscribe(params => {
       this.setStatistics();
-  });
+    });
+
+
+    let chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Basic Column Chart in Angular"
+      },
+      data: [{
+        type: "column",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+    });
+
+    chart.render();
   }
 
   setStatistics() {
